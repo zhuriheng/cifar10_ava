1c2854be341f213b17d095a93f32d3b2f65b2d8a
zhuriheng <35758033+zhuriheng@users.noreply.github.com>
2018-02-26 11:57:33 +0800

    add "move the file default.json"

--- start.sh
+++ start.sh
@@ -8,5 +8,9 @@ cd /workspace/model
 wget http://data.mxnet.io/models/imagenet/resnet/50-layers/resnet-50-0000.params
 wget http://data.mxnet.io/models/imagenet/resnet/50-layers/resnet-50-symbol.json
 
+echo "move the file default.json to the folder /workspace/params"
+mkdir /workspace/params
+mv default.json /workspace/params
+
 echo "start to run training script"
 python /workspace/runs/cifar10_ava/training.py
